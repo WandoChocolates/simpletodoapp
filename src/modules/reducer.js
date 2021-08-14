d0e8fd351fee57c41895725ba0fa7539ca4bf548
WandoChocolates <ellytitus50@gmail.com>
2021-08-14 14:55:29 +0800

    make functional

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -10,16 +10,16 @@ const todoReducer = (state = defaultState, action) => {
     case ACTIONS.Types.CREATE_ITEM: {
       console.log(action);
 
-      let item = action.payload;
-      let newItem = { id: state.items.length + 1, description: item };
-      let newState = _.cloneDeep(state);
+      const item = action.payload;
+      const newItem = { id: state.items.length + 1, description: item };
+      const newState = _.cloneDeep(state);
       newState.items.push(newItem);
       return newState;
     }
 
     case ACTIONS.Types.DELETE_ITEM: {
-      let newState = _.cloneDeep(state);
-      let index = _.findIndex(newState.items, { id: action.payload });
+      const newState = _.cloneDeep(state);
+      const index = _.findIndex(newState.items, { id: action.payload });
       newState.items.splice(index, 1);
       return newState;
     }
--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   withStyles,
   List,
@@ -29,17 +29,18 @@ const styles = theme => ({
   }
 });
 
-class ToDO extends Component {
-  state = {};
+const ToDO = (props) => {
+  const [state, setState] = useState({});
+  const { classes } = props;
 
-  generate = () => {
-    return this.props.items.map(item => (
+  const generate = () => {
+    return props.items.map(item => (
       <ListItem key={item.id}>
         <ListItemText primary={item.description} />
         <ListItemSecondaryAction>
           <IconButton
             aria-label="Delete"
-            onClick={this.handleDelete}
+            onClick={handleDelete}
             value={item.id}
           >
             <DeleteIcon />
@@ -49,59 +50,55 @@ class ToDO extends Component {
     ));
   };
 
-  handleSubmit = event => {
-    // console.log(this.state.item);
-    this.setState({ item: "" });
-    if (this.state.item !== "") {
+  const handleSubmit = event => {
+    // console.log(state.item);
+    setState({ item: "" });
+    if (state.item !== "") {
       // add the item to the store
-      this.props.createItem(this.state.item);
+      props.createItem(state.item);
     }
     event.preventDefault();
   };
   
-  handleDelete = event => {
+  const handleDelete = event => {
     // delete the item from the store
-    this.props.deleteItem(event.target.value);
+    props.deleteItem(event.target.value);
   };
-  handleChange = event => {
-    this.setState({
+  const handleChange = event => {
+    setState({
       [event.target.name]: event.target.value
     });
   };
 
-  render() {
-    const { classes } = this.props;
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
-            <FormControl>
-              <TextField
-                label="New Task"
-                id="margin-dense"
-                value={this.state.item}
-                className={classes.textField}
-                margin="dense"
-                name="item"
-                onChange={this.handleChange}
-              />
-            </FormControl>
-            <FormControl>
-              <Button>Add</Button>
-            </FormControl>
-          </form>
-        </div>
-        <div>
-          <Grid item container justify="space-evenly" alignItems="center">
-            <div className={classes.demo}>
-              <List dense={false}>{this.generate()}</List>
-            </div>
-          </Grid>
-        </div>
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <FormControl>
+            <TextField
+              label="New Task"
+              id="margin-dense"
+              value={state.item}
+              className={classes.textField}
+              margin="dense"
+              name="item"
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <Button type= "submit">Add</Button>
+          </FormControl>
+        </form>
       </div>
-    );
-  }
+      <div>
+        <Grid item container justify="space-evenly" alignItems="center">
+          <div className={classes.demo}>
+            <List dense={false}>{generate()}</List>
+          </div>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
